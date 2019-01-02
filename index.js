9b49946919cee949c886b75ee838255a52b6fa98
michaelrhodes <spam@michaelrhod.es>
2019-01-02 10:13:29 +1000

    Add index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+module.exports = {
+  clone: require('./clone'),
+  extend: require('./extend'),
+  mutate: require('./mutate'),
+  omit: require('./omit'),
+  pick: require('./pick'),
+  get: require('./get'),
+  set: require('./set'),
+  expand: require('./expand'),
+
+  each: require('./each'),
+  kmap: require('./kmap'),
+  vmap: require('./vmap'),
+  kfilter: require('./kfilter'),
+  vfilter: require('./vfilter')
+}
