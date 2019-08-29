46a18f7b1cddb600f2436b81421975f5bd4f6ded
michaelrhodes <spam@michaelrhod.es>
2019-08-29 18:29:07 +1000

    Make expand more compact

--- expand.js
+++ expand.js
@@ -6,8 +6,7 @@ function expand (oo, o) {
   var i = 0, l = k.length
   var j, m, sk, so
   for (; i < l; i++) {
-    so = oo
-    sk = k[i].split('.')
+    so = oo, sk = k[i].split('.')
     j = 0, m = sk.length - 1
     for (; j < m; j++)
       so = so[sk[j]] = so[sk[j]] || {}
